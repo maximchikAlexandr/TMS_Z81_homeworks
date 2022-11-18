75ae3b47d8d416289e2c89c852b4a3059721d267
Alexandr <maximchik.alexandr@yandex.ru>
2022-11-18 22:07:28 +0300

    Add operator 'is'

--- lesson_2/task_1_2_3.py
+++ lesson_2/task_1_2_3.py
@@ -1,19 +1,16 @@
 # task 1
-var1 = var2 = var3 = 1
-assert var1 == var2 == var3
-assert id(var1) == id(var2) == id(var3)
+var1 = var2 = var3 = 2
+assert var1 == var2 == var3 and \
+        var1 is var2 is var3
 
 # task 2
 var4, var5 = 3, 3.0
-assert var4 == var5
-assert id(var4) != id(var5)
+assert var4 == var5 and var4 is not var5
 
 # task 3
-var2 = float(var2)
-var3 = bool(var3)
-assert var1 == var2 == var3
-assert id(var1) != id(var2) != id(var3)
+var1, var2 = float(var1), float(var2)
+assert var1 == var2 == var3 and \
+    var1 is not var2 is not var3
 
 var5 = int(var5)
-assert var4 == var5
-assert id(var4) == id(var5)
+assert var4 == var5 and var4 is var5
