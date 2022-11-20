2ff2a95dfb710cfb2dd6c40671004f7d698d0cc5
Alexandr <maximchik.alexandr@yandex.ru>
2022-11-20 19:29:07 +0300

    Add more 'assert'

--- lesson_2/task_1_2_3.py
+++ lesson_2/task_1_2_3.py
@@ -1,7 +1,7 @@
 # task 1
 var1 = var2 = var3 = 2
-assert var1 == var2 == var3 and \
-    var1 is var2 is var3
+assert var1 == var2 == var3
+assert var1 is var2 is var3
 
 # task 2
 var4, var5 = 3, 3.0
@@ -9,8 +9,9 @@ assert var4 == var5 and var4 is not var5
 
 # task 3
 var1, var2 = float(var1), float(var2)
-assert var1 == var2 == var3 and \
-    var1 is not var2 is not var3
+assert var1 == var2 == var3
+assert var1 is not var2 is not var3
 
 var5 = int(var5)
-assert var4 == var5 and var4 is var5
+assert var4 == var5
+assert var4 is var5
