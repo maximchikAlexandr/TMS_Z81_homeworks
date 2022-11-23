6b727d2c3b7a46f722fd932170d7c213b855ffb2
Alexandr <maximchik.alexandr@yandex.ru>
2022-11-23 20:14:24 +0300

    Update task_2_3.py

--- lesson_3/task_4.py
+++ lesson_3/task_4.py
@@ -1,19 +1,13 @@
-# Ввести с клавиатуры целое число n. Получить сумму кубов всех целых чисел от 1 до
-# n включая n). Реализовать в 2-х вариантах: используя цикл while и цикл for
-
 n = int(input())
 seq = range(1, n + 1)
-result1, result2 = 0, 0
-
+result1, result2, i = 0, 0, 0
 
 for num in seq:
     result1 += num ** 3
-
-i = 0
 
 while i < len(seq):
     result2 += seq[i] ** 3
     i += 1
 
 assert result1 == result2
-print(result1, result2)
+print(result1, result2, sep='\n')
