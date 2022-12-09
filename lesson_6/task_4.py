6675a31ccc6d3ce1c69259a34b4ea3c97ed4a492
Alexandr <maximchik.alexandr@yandex.ru>
2022-12-09 23:19:33 +0300

    Refactoring task 4-5

--- lesson_6/task_4.py
+++ lesson_6/task_4.py
@@ -6,20 +6,19 @@ with open("data.json", "r", encoding="utf-8") as file:
     src_dict = json.loads(json_str)
 
 phones = ["098-98-51", "098-98-52", "098-98-53", "098-98-54", "098-98-55"]
-fieldnames = ["id", "name", "age", "phone"]
 src_list = []
 
 for id_, name_and_age, phone in zip(src_dict.keys(), src_dict.values(), phones):
     src_list.append(
         {
-            fieldnames[0]: id_,
-            fieldnames[1]: name_and_age[0],
-            fieldnames[2]: name_and_age[1],
-            fieldnames[3]: phone,
+            "id": id_,
+            "name": name_and_age[0],
+            "age": name_and_age[1],
+            "phone": phone,
         }
     )
 
 with open("data.csv", "w", encoding="utf-8") as file:
-    csv_writer = csv.DictWriter(file, fieldnames)
+    csv_writer = csv.DictWriter(file, fieldnames=src_list[0])
     csv_writer.writeheader()
-    csv_writer.writerows(src_list)
+    csv_writer.writerows(rowdicts=src_list)
--- lesson_6/task_5.py
+++ lesson_6/task_5.py
@@ -8,7 +8,7 @@ with open("data.csv", "r", encoding="utf-8") as file:
     src_list = list(csv.reader(file))
 
 # получаем лист, где каждый элемент - лист со значениями для одной строки в экселе
-row_headers = [""] + [f"Person {indx}" for indx in range(len(src_list))]
+row_headers = [""] + [f"Person {indx}" for indx in range(1, len(src_list))]
 
 
 def filter_list_by_indx(source: list[list[str]], indx: int) -> list[str]:
