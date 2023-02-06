23f17b6e672f93f7e99d9c157421eca41e8aca1d
Alexandr <maximchik.alexandr@yandex.ru>
2023-02-06 15:00:19 +0300

    lesson 17: PEP8

--- lesson_17_patterns/client.py
+++ lesson_17_patterns/client.py
@@ -1,17 +1,18 @@
 import socket
+
 from interface import SocketInterface
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(SocketInterface.address)
-message_in = ""
+MESSAGE_IN = ""
 
-while message_in != '0':
-    message_out = client_socket.recv(2048).decode('utf-8')
-    if message_out.startswith('info:'):
+while MESSAGE_IN != "0":
+    message_out = client_socket.recv(2048).decode("utf-8")
+    if message_out.startswith("info:"):
         print(f"\nShop:\n{message_out[5:]}\n")
     else:
         print(f"\nShop:\n{message_out}\n")
-        message_in = input("Customer: ")
-        client_socket.send(message_in.encode('utf-8'))
+        MESSAGE_IN = input("Customer: ")
+        client_socket.send(MESSAGE_IN.encode("utf-8"))
 
 client_socket.close()
