96a27484b0ce77b405980f0c3236199ee9de9c92
Alexandr <maximchik.alexandr@yandex.ru>
2023-02-25 18:50:14 +0300

    lesson 23: refactoring class HoroscopeSource

---
+++ lesson_23_horoscope/services.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from typing import Dict
+
+import requests
+import xmltodict
+
+
+class HoroscopeSource:
+    __HOROSCOPE_SOURCE_URL = "https://ignio.com/r/export/utf/xml/daily/com.xml"
+
+    def __init__(self) -> None:
+        self.__get_horoscope_from_internet()
+
+    def get_horoscope(self, zodiac_name: str) -> Dict[str, str]:
+        """Проверяем актуальность гороскопов и возвращаем гороскоп
+        по знаку зодиака"""
+        if not self.__is_actual_horoscope():
+            self.__get_horoscope_from_internet()
+        return self.__create_horoscope_dict(zodiac_name)
+
+    def __is_actual_horoscope(self) -> bool:
+        today = self.__horoscopes["horo"]["date"]["@today"]
+        today = datetime.strptime(today, "%d.%m.%Y").date()
+        return today == datetime.now().date()
+
+    def __get_horoscope_from_internet(self) -> None:
+        xml_str = requests.get(self.__HOROSCOPE_SOURCE_URL, timeout=5).text
+        self.__horoscopes = xmltodict.parse(xml_str)
+
+    def __create_horoscope_dict(self, zodiac_name: str) -> Dict[str, str]:
+        horoscope = {}
+        for day, hrscp in self.__horoscopes["horo"][zodiac_name].items():
+            horoscope[self.__horoscopes["horo"]["date"][f"@{day}"]] = hrscp
+        return horoscope
+
+
+def main():
+    zodiac_signs = [
+        "aries",
+        "taurus",
+        "gemini",
+        "cancer",
+        "leo",
+        "virgo",
+        "libra",
+        "scorpio",
+        "sagittarius",
+        "capricorn",
+        "aquarius",
+        "pisces",
+    ]
+    horoscope_src = HoroscopeSource()
+    horoscope = horoscope_src.get_horoscope(zodiac_signs[2])
+    print(horoscope)
+
+
+if __name__ == "__main__":
+    main()
