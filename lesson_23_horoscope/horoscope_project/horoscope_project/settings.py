8a2ebb618879fa927b8ef19e0871964d590ce272
Alexandr <maximchik.alexandr@yandex.ru>
2023-03-07 00:25:50 +0300

    lesson 24: add docker files

--- lesson_23_horoscope/horoscope_project/horoscope_project/settings.py
+++ lesson_23_horoscope/horoscope_project/horoscope_project/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = "horoscope_project.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get("DB_ENGINE"),
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
         'HOST': os.environ.get("DB_HOST"),
         'PORT': os.environ.get("DB_PORT"),
     }
